94a0eb80fcecd70801bfffa14b95c04ddaa70e8c
tomasernst <ternst@uc.cl>
2022-06-21 23:01:17 -0400

    Mejora connection

--- config/data.php
+++ config/data.php
@@ -4,7 +4,7 @@
     $password = 'ernst123';
     $db_name = 'grupo7e3';
 
-    // $user = 'grupo122';
-    // $password = '...?...';
-    // $db_name = 'grupo122e3';
+    $user2 = 'grupo122';
+    $password2 = '...?...';
+    $db_name2 = 'grupo122e3';
 ?>
