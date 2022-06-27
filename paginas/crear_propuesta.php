6fc2d9326b02d85dda81d9798dd5d0bc37c63b9c
Ignacia González Goicovich <ignacia.gonzlez@uc.cl>
2022-06-26 20:18:33 -0400

    para la pag de las companias

---
+++ paginas/crear_propuesta.php
@@ -0,0 +1,29 @@
+<?php
+
+        require("conection.php");
+    
+        $estado = 'pendiente';
+        // obtengo los datos del formulario 
+        $propuesta_vuelo_id = $_POST["propuesta_vuelo_id"] ;
+        $codigo = $_POST["codigo"] ;
+        $fecha_salida = $_POST["fecha_salida"] ;
+        $hora_salida = $_POST["hora_salida"] ;
+        $fecha_llegada = $_POST["fecha_llegada"] ;
+        $hora_llegada = $_POST["hora_llegada"] ;
+        $aerodromo_salida_id = $_POST["aerodromo_salida_id"] ;
+        $aerodromo_llegada_id = $_POST["aerodromo_llegada_id"] ;
+        $codigo_aeronave = $_POST["codigo_aeronave"] ;
+        $fecha_envio_propuesta = $_POST["fecha_envio_propuesta"];
+        $codigo_compania = $_POST["codigo_compania"];
+
+        $query = "INSERT INTO propuestas (propuesta_vuelo_id, estado, codigo,  fecha_salida, hora_salida, fecha_llegada, hora_llegada, aerodromo_salida_id, aerodromo_llegada_id, codigo_aeronave, fecha_envio_propuesta)
+        VALUES ($propuesta_vuelo_id, $estado, $codigo, $fecha_salida, $hora_salida, $fecha_llegada, $hora_llegada, $aerodromo_salida_id, $aerodromo_llegada_id, $codigo_aeronave, $fecha_envio_propuesta);";
+        $result = $db2 -> prepare($query);
+        $result -> execute();
+
+        $query2 = "INSERT INTO propuesta_compania (propuesta_vuelo_id, codigo_compania)
+        VALUES ($propuesta_vuelo_id, $codigo_compania);";
+        $result2 = $db2 -> prepare($query2);
+        $result2 -> execute();
+
+    ?>
---
+++ paginas/propuestas.php
@@ -0,0 +1,126 @@
+<?php include("/home/grupo7/Sites/templates/header.html"); ?>
+<h3>COMPAÑIA</h3>
+
+
+<body>
+<?php   
+        session_start();
+        require("/home/grupo7/Sites/config/connection.php");
+        // $username = $_GET["username"] ;
+        // cambiar a nombre_compania = usuario (de tipo compañía activo) LATAM era para probar
+        $username = $_SESSION['username'];
+        $query = "SELECT * FROM propuestas WHERE propuesta_vuelo_id IN (
+            SELECT propuesta_vuelo_id FROM propuesta_compania WHERE codigo_compania = (
+                SELECT codigo_compania FROM compania WHERE nombre_compania = '$username'
+            )
+        );";
+        $result = $db2 -> prepare($query);
+        $result -> execute();
+
+        $data = $result -> fetchAll();
+    ?>
+
+<!-- if vuelo aprobado -->
+<h2> Propuestas aprobadas </h2>
+
+    <table>
+        <tr>
+            <th> ID </th>
+            <th> Estado </th>
+            <th> Codigo </th>
+            <th> Fecha salida </th>
+            <th> Hora salida </th>
+            <th> Fecha llegada </th>
+            <th> Hora llegada </th>
+            <th> ID aerodromo salida </th>
+            <th> ID aerodromo llegada </th>
+            <th> Codigo aeronave </th>
+            <th> Fecha envio propuesta </th>
+        </tr>
+
+        <?php
+            foreach ($data as $d) {
+                if ($d[1] == "aceptado"){
+                    echo "<tr>
+                    <td>$d[0]</td>
+                    <td>$d[1]</td>
+                    <td>$d[2]</td>
+                    <td>$d[3]</td>
+                    <td>$d[4]</td>
+                    <td>$d[5]</td>
+                    <td>$d[6]</td>
+                    <td>$d[7]</td>
+                    <td>$d[8]</td>
+                    <td>$d[9]</td>
+                    <td>$d[10]</td>
+                    <td>$d[11]</td>
+                         </tr>";}
+            }
+        ?>
+
+    </table>
+    <!--  -->
+
+    <!-- elif vuelo rechazado -->
+    <h2> Propuestas Rechazadas </h2>
+
+    <table>
+        <tr>
+            <th> ID </th>
+            <th> Estado </th>
+            <th> Codigo </th>
+            <th> Fecha salida </th>
+            <th> Hora salida </th>
+            <th> Fecha llegada </th>
+            <th> Hora llegada </th>
+            <th> ID aerodromo salida </th>
+            <th> ID aerodromo llegada </th>
+            <th> Codigo aeronave </th>
+            <th> Fecha envio propuesta </th>
+        </tr>
+
+        <?php
+            foreach ($data as $d) {
+                if ($d[1] == "rechazado"){
+                    echo "<tr>
+                    <td>$d[0]</td>
+                    <td>$d[1]</td>
+                    <td>$d[2]</td>
+                    <td>$d[3]</td>
+                    <td>$d[4]</td>
+                    <td>$d[5]</td>
+                    <td>$d[6]</td>
+                    <td>$d[7]</td>
+                    <td>$d[8]</td>
+                    <td>$d[9]</td>
+                    <td>$d[10]</td>
+                    <td>$d[11]</td>
+                         </tr>";}
+            }
+        ?>
+
+    </table>
+    
+
+    <?php include("/home/grupo7/Sites/templates/footer.html"); ?>
+
+<?php // ?>
+</html>
+
+<h3>Crear Propuesta</h3>
+
+<form action="crear_propuesta.php" method="post">
+    ID propuesta: <input type="text" name="propuesta_vuelo_id"><br>
+    codigo: <input type="text" name="codigo"><br>
+    fecha salida: <input type="text" name="fecha_salida"><br>
+    fecha salida: <input type="text" name="fecha_salida"><br>
+    hora salida: <input type="text" name="hora_salida"><br>
+    fecha llegada: <input type="text" name="fecha_llegada"><br>
+    hora llegada: <input type="text" name="hora_llegada"><br>
+    aerodromo salida_id: <input type="text" name="aerodromo_salida_id"><br>
+    aerodromo llegada id: <input type="text" name="aerodromo_llegada_id"><br>
+    codigo aeronave: <input type="text" name="codigo_aeronave"><br>
+    fecha envio propuesta: <input type="text" name="fecha_envio_propuesta"><br>
+    codigo compania: <input type="text" name="codigo_compania"><br>
+    <input type="submit" value="Enviar">
+</form>
